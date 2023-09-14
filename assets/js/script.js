f4cda9ee4dd67699236d3cd340062c3677e27ef2
Md Amanullah <amn3242@gmail.com>
2023-09-14 23:02:04 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const apiKey = "7a4e2b0ed9e32caebd9f39509503a84f";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
+    const weatherIcon = data.weather[0].main;
+    document.querySelector(".weather-icon").src =
+      "assets/images/" + weatherIcon.toLowerCase() + ".png";
+
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
+
+checkWeather(city);
